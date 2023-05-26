cc11f306a68ba215205e605dc9f41b9d224e8b60
Marconi Pereira <marconiaula@gmail.com>
2023-05-26 19:02:00 -0300

    modificacao do form para componete

--- src/pages/curriculo/CadastrarInformacoes/CadastrarInformacoes.tsx
+++ src/pages/curriculo/CadastrarInformacoes/CadastrarInformacoes.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
+
+import Input from "../../../components/forms/Input";
+import Textarea from "../../../components/forms/Textarea";
 
 import styles from "./CadastrarInformacoes.module.css"
 
@@ -44,51 +47,35 @@ const CadastrarInformacoes: React.FC = () => {
                 {({errors, touched}) => (
                     <Form action="" className={styles.form}>
 
-                        <h2 className={styles.formGroup}>Informações Pessoais</h2>
+                        <h2 className={styles.formGroup}>Cadastrar Infromações</h2>
 
-                        <fieldset className={styles.formGroup}>
-                            <label htmlFor="foto" className={styles.label}>Foto</label>
-                            <Field 
-                                type="text"
-                                id="foto"
-                                name="foto"
-                                className={`${styles.input} ${touched.foto && errors.foto && styles.error}`}
-                            />
-                            <ErrorMessage name="foto" component="div" className={styles.errorMsg} />
-                        </fieldset>
+                        <Input
+                            label="Foto"
+                            name="foto"
+                            errors={errors.foto}
+                            touched={touched.foto}
+                        />
 
-                        <fieldset className={styles.formGroup}>
-                            <label htmlFor="nome" className={styles.label}>Nome</label>
-                            <Field 
-                                type="text"
-                                id="nome"
-                                name="nome"
-                                className={`${styles.input} ${touched.foto && errors.foto && styles.error}`}
-                            />
-                            <ErrorMessage name="nome" component="div" className={styles.errorMsg} />
-                        </fieldset>
-
-                        <fieldset className={styles.formGroup}>
-                            <label htmlFor="cargo" className={styles.label}>Cargo</label>
-                            <Field 
-                                type="text"
-                                id="cargo"
-                                name="cargo"
-                                className={`${styles.input} ${touched.foto && errors.foto && styles.error}`}
-                            />
-                            <ErrorMessage name="cargo" component="div" className={styles.errorMsg} />
-                        </fieldset>
-
-                        <fieldset className={styles.formGroup}>
-                            <label htmlFor="resumo" className={styles.label}>Resumo</label>
-                            <Field 
-                                as="textarea"
-                                id="resumo"
-                                name="resumo"
-                                className={`${styles.textarea} ${touched.foto && errors.foto && styles.error}`}
-                            />
-                            <ErrorMessage name="resumo" component="div" className={styles.errorMsg} />
-                        </fieldset>
+                        <Input
+                            label="Nome"
+                            name="nome"
+                            errors={errors.nome}
+                            touched={touched.nome}
+                        />
+                        
+                        <Input
+                            label="Cargo"
+                            name="cargo"
+                            errors={errors.cargo}
+                            touched={touched.cargo}
+                        />
+                        
+                        <Textarea 
+                            label="Resumo"
+                            name="resumo"
+                            errors={errors.resumo}
+                            touched={touched.resumo}
+                        />
 
                         <button type="submit" className={styles.button}>Salvar</button>
 
